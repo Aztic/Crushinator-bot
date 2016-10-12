19c009214478b4444abb5c0ada8563eb7c81e362
Jhosevic Castañeda <zetsubou97@gmail.com>
2016-10-12 18:47:16 -0400

    Update Crushinator.py

--- Crushinator.py
+++ Crushinator.py
@@ -1,4 +1,5 @@
 from Anilist_date_search import next_anime
+import commands
 import asyncio
 import discord
 import os
@@ -9,6 +10,7 @@ CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 al_client = next_anime.client
 d_client = discord.Client()
+r_client = commands.client
 
 @d_client.event
 async def on_ready():
@@ -21,17 +23,37 @@ async def on_ready():
 @d_client.event
 async def on_message(message):
 	if message.content.startswith('$next'):
-		anime = message.content.split("$next ")[1]
-		path = 'anime/search/' + anime
-		an_id = next_anime.search_thing(anime,path,al_client)
+		search = message.content.split("$next ")[1]
+		path = 'anime/search/' + search
+		an_id = next_anime.search_thing(search,path,al_client)
 		if an_id == -1:
-			await d_client.send_message(message.channel, anime + ' is not airing')
+			await d_client.send_message(message.channel, search + ' is not airing')
 		elif not an_id:
 			await d_client.send_message(message.channel, 'Not found, write it properly')
 		else:
 			secs = next_anime.search_with_id(an_id,al_client)
-			the_string = next_anime.date_things(secs,0,anime,1)
+			the_dict = next_anime.date_things(secs,0,search,1)
+			the_string = ''
+			order = ['days', 'hours', 'minutes', 'seconds']
+			for i in order:
+				if the_dict[i] != '0':
+					the_string += the_dict[i] + ' ' + i + ' '
+			print(search)
 			await d_client.send_message(message.channel, the_string)
+
+	if message.content == '$free week':
+		await d_client.send_message(message.channel, ', '.join(commands.free_week(r_client)))
+
+	if message.content.startswith('$mastery'):
+		search = message.content.split("$mastery")[1].split()
+		region = search[0]
+		champ = search[1]
+		summoner = ''.join(search[2:len(search)])
+		data = commands.mastery_level(champ,region,summoner,r_client)
+		if data == -1:
+			await d_client.send_message(message.channel, 'Not found')
+		else:
+			await d_client.send_message(message.channel, summoner + ' has lvl ' + str(data['championLevel']) + ' with ' + champ + ' and has ' + str(data['championPoints']) + ' points with it')
 
 
 d_client.run(BOT_TOKEN)
